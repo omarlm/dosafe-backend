7fc3679fd5d9cb3c70ba2d613afcf9ba407e30b4
Omar Lorenzo <omarlm@MacBook-Pro-de-Omar.local>
2024-04-09 13:19:29 +0100

    Change CORS v2

--- src/app.js
+++ src/app.js
@@ -2,9 +2,21 @@
 const express = require('express')
 const cors = require('cors');
 const app = express()
-const routes = require('./routes') // Asegúrate de ajustar la ruta según la estructura de tu proyecto
+const routes = require('./routes')
 
-app.use(cors());
+const whitelist = ['https://dazzling-figolla-4b1e5d.netlify.app/']
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('No permitido por CORS'))
+        }
+    },
+}
+
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use(routes)
 
